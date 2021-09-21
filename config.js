e91b02060b10326d093710bbecccad643d23c6a7
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-09-20 22:36:57 -0700

    updated config to support docs

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 module.exports = {
     rest: {
         port : {{{PORT}}},
+        docs : {
+            '{{{SERVICE_NAME}}}' : '{{{PREFIX}}}/spec'
+        },
+        schemas : {
+            '{{{SERVICE_NAME}}}' : '{{{PREFIX}}}/schema'
+        }
     },
     JWT_TOKEN_SECRET : `${process.env.JWT_TOKEN_SECRET}`
 }
